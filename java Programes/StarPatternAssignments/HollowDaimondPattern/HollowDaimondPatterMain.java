4a9d237ede159ba1c8129f93b2f88d664843bf39
wakodikarShital <shitalwakodikar436@gmail.com>
2023-07-10 23:57:38 +0530

    Hollow Daimond Pattern

--- java Programes/StarPatternAssignments/HollowDaimondPattern/HollowDaimondPatterMain.java
+++ java Programes/StarPatternAssignments/HollowDaimondPattern/HollowDaimondPatterMain.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class HollowDaimondPatterMain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the number of rows for Hollow Daimond star pattern : ");
+        System.out.println("Enter the number of size for Hollow Daimond star pattern : ");
         int size = scanner.nextInt();
         HollowDaimondStarPattern hollowDaimondStarPattern = new HollowDaimondStarPattern();
         hollowDaimondStarPattern.daimondPattern(size);

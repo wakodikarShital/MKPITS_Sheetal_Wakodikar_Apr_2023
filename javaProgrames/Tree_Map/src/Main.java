ac3b59e42330be3a425e17b0fb6490e0cfb6ab96
wakodikarShital <shitalwakodikar436@gmail.com>
2023-09-28 22:52:16 +0530

    Tree map programmes

---
+++ javaProgrames/Tree_Map/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        TreeMapClass treeMapClass = new TreeMapClass();
+        treeMapClass.treeMap();
+    }
+}
---
+++ javaProgrames/Tree_Map/src/TreeMapClass.java
@@ -0,0 +1,19 @@
+import java.util.Iterator;
+import java.util.TreeMap;
+
+public class TreeMapClass {
+    public void treeMap(){
+        TreeMap<Integer,String> treeMap = new TreeMap<>();
+        treeMap.put(1 , "shital");
+        treeMap.put(2 , "neha");
+        treeMap.put(7 , "mrunali");
+        treeMap.put(3 , "trupti");
+
+        treeMap.put(3 , "taniya");
+        System.out.println(treeMap);
+
+
+
+
+    }
+}

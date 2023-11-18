156a6e73071b6562b47a625cc315675425b3e6f0
wakodikarShital <shitalwakodikar436@gmail.com>
2023-11-18 14:56:19 +0530

    Setter injection by dependent object

---
+++ javaProgrames/Advance_Java/Spring/Spring_Project/SetterInjectionByDependentObject/src/employee/Address.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package employee;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Address {
+    private String city;
+    private String state;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    @Override
+    public String toString() {
+        return  "city = " + city + ","
+                + "\nstate = " + state ;
+    }
+    
+    
+    
+}
---
+++ javaProgrames/Advance_Java/Spring/Spring_Project/SetterInjectionByDependentObject/src/employee/Employee.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package employee;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Employee {
+    
+    private int id;
+    private String name;
+    private Address address;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+    
+    public void displayInfo(){
+        System.out.println("Id = " + id);
+        System.out.println("Name = " + name);
+        System.out.println("Address : ");
+        System.out.println(address);
+    }
+    
+    
+}

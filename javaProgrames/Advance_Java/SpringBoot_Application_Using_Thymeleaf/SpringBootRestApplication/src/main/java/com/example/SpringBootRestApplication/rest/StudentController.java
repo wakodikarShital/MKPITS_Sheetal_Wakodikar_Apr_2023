f5eb9fbeaa7b6775d81b25099b6878f7440a3952
wakodikarShital <shitalwakodikar436@gmail.com>
2023-12-05 22:38:49 +0530

    data rest api and spring boot application using thymeleaf

---
+++ javaProgrames/Advance_Java/SpringBoot_Application_Using_Thymeleaf/SpringBootRestApplication/src/main/java/com/example/SpringBootRestApplication/rest/StudentController.java
@@ -0,0 +1,44 @@
+package com.example.SpringBootRestApplication.rest;
+
+import com.example.SpringBootRestApplication.entity.Student;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class StudentController {
+    @RequestMapping ("/hello")
+    public String display(Model model){
+        model.addAttribute("name","Shital Wakodikar");
+        return "hello";
+    }
+
+    @GetMapping("/infoForm")
+    public String form(Model model){
+//        List<Student> studentList = new ArrayList<>();
+//        model.addAttribute("students",studentList);
+        return "info";
+    }
+
+    @GetMapping("/process")
+      public String processData(HttpServletRequest request , Model model){
+        String rollNumber= request.getParameter("roll_number");
+        String name=request.getParameter("name");
+        String city=request.getParameter("city");
+        String address=request.getParameter("address");
+
+        model.addAttribute("rollNumber",rollNumber);
+        model.addAttribute("name",name);
+        model.addAttribute("city",city);
+        model.addAttribute("address",address);
+
+        return "processData";
+    }
+
+
+}
